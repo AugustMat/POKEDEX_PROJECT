7b360d7f6b1750ec256402b046484c41da74d939
Augusto <mateus_system@hotmail.com>
2024-03-26 14:57:48 -0300

    new model to modal

--- assets/js/pokemon-model.js
+++ assets/js/pokemon-model.js
@@ -6,3 +6,27 @@ class Pokemon {
     types = [];
     photo;
 }
+
+class PokemonDetails {    
+    constructor(name, number, height, weight, status = [], types = [], photo){
+        this.name = name;
+        this.number = number;
+        this.height = height;
+        this.weight = weight;
+        this.status = status;
+        this.types = types;
+        this.photo = photo;
+    }
+}
+
+// class PokemonDetails {
+//     constructor(name, order, height, weight, status = [], types = [], photo){
+//         this.name = name;
+//         this.order = order;
+//         this.height = height;
+//         this.weight = weight;
+//         this.status = status;
+//         this.types = types;
+//         this.photo = photo;
+//     }
+// }
